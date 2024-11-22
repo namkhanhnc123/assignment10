f685f07fac3826e19e9b7426318cbcfac960670b
namkhanhnc123 <21020267@vnu.edu.vn>
2024-11-22 15:42:31 +0700

    Update routing to use HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/Login';
 import HomePage from './components/Home';
 import AboutPage from './components/About';
